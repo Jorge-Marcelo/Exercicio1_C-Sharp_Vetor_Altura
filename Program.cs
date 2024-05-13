953fa1f764c62dd63cbebbfa7d85d2e6c1d3d41d
Jorge-Marcelo <jorge3258@hotmail.com>
2024-05-13 13:05:13 -0300

    Primeiro Commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_Vetor_Media_Altura
+{
+ internal class Program
+ {
+  static void Main(string[] args)
+  {
+
+
+   Console.Write("Digite o numero de indices maximos do vetor: ");
+   int x = int.Parse(Console.ReadLine());
+   double[] Altura = new double[x];
+
+   for (int iterador = 0; iterador < x; iterador++){
+   Altura[iterador] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+   }
+
+   double soma = 0.0;
+   for(int iterador = 0; iterador < x; iterador++){
+   soma += Altura[iterador];
+   }
+
+   double media = soma / x;
+   Console.WriteLine($"Media das alturas: {media.ToString("F2",CultureInfo.InvariantCulture)}");
+
+  }
+ }
+}
